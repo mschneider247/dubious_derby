6a2310e754f36ad04903205ee533f31d2aac0c0b
Michael <mschneider247@gmail.com>
2020-03-05 16:40:39 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'typeface-roboto';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Itsa Material Button!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
