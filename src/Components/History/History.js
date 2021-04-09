48c1df9042b1ace9706966d998974d404fd31781
Michael <mschneider247@gmail.com>
2021-04-09 12:31:05 -0600

    Allow History component to display movie titles from Airtable

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const buildMovies = (movies) => {
+    let renderMovies = movies.map((movie, index) => {
+      return (
+        <div>
+          {movie.fields.title}
+        </div>
+      )
+    })
+    return renderMovies
+  }
 
 const History = (props) => {
-  
+
+  console.log("Test hello, inside History, fuck ya")
+  console.log("What is props.movies.records", props.movies.records)
+  // an array of movie objects, jack pots
+
   return (
     <section>
-      <h2>Movie History</h2>
+      <div>
+        {buildMovies(props.movies.records)}
+      </div>
     </section>
   )
 }
